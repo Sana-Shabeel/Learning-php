4010e79cadb70983dde4af8123e52eea9282e8ad
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-19 08:32:09 +0300

    setcookie func

--- index.php
+++ index.php
@@ -1,34 +1,9 @@
-
-
-
-
-
 <?php
 
-include("header.html");
- 
 
-?>
+setcookie("name", "John Watkin", time() + (86400 * 2) , "/"); 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    This is the home page <br>
-    Stuff about the home page can go here <br>
-</body>
-</html>
-
-
-<?php
-
-include("footer.html");
  
 
 ?>
