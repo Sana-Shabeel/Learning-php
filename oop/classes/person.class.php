0bc8cf4e8f1657dcea77043b868457c71ee48eb3
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-22 11:30:55 +0300

    Add OOP PHP classes folder

---
+++ oop/classes/person.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class Person 
+{
+    
+    // Properties
+    public $name;
+    public $eyeColor;
+    public $age;
+    
+    
+    public static $drinkingAge = 21;
+
+    
+    public function __construct($name, $eyeColor, $age) {
+      $this->name = $name;
+      $this->eyeColor = $eyeColor;
+      $this->age = $age;
+        
+    }
+    
+    // Methods
+    public function setName($name) {
+        $this->name = $name;
+    }
+    
+    public static function setDrinkingAge($newDA) {
+        self::$drinkingAge = $newDA;
+    }
+
+}
+
+
+class Counter {
+  public static $count = 0;
+
+  public function __construct() {
+      self::$count++;  // Accessing the static property using self::
+  }
+}
