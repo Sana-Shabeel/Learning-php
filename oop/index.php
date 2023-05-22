20190db52d2c5cbbc52fff60778f307b5218f40b
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-22 11:31:48 +0300

    Add OOP PHP load classes automatically

--- oop/index.php
+++ oop/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'includes/newclass.inc.php';
+include 'includes/autoloader.inc.php';
 
 
 
@@ -12,5 +12,28 @@ $object = new Person("John", "blue", 28);
   $object->setName("John Doe");
   
   echo $object->name;
+  
+  echo "<br>";
+  
+  echo Person::$drinkingAge;
+  
+  echo "<br>";
+  
+  Person::setDrinkingAge(18);
+  
+  echo Person::$drinkingAge;
+  
+  
+
+  echo "<br>";
+
+// Creating instances of the Counter class
+$counter1 = new Counter();
+$counter2 = new Counter();
+$counter3 = new Counter();
+
+// Accessing the static property
+echo Counter::$count;  // Output: 3
+  
 
 ?>
