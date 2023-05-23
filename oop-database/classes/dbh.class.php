e0cb4ff931237ce973f2a0021b5281d74d6c353a
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-23 20:45:26 +0300

    Add db connection

--- oop-database/classes/dbh.class.php
+++ oop-database/classes/dbh.class.php
@@ -10,15 +10,20 @@ class Dbh
     private $dbTable = "php_docker";
     
     // connect to db using mysqli
-    protected function connect($query)
+    protected function connect()
     {
         $conn = new mysqli($this->serviceName, $this->userName, $this->pwd, $this->dbTable);
-        $response = mysqli_query($conn, $query);
-        return $response;
+        
+        // var_dump($conn->connect_error);
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+        return $conn;
 
     }
-    
-    
+ 
     
 }
 
