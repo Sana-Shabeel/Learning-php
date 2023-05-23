e2c9b291db5a28e448b7126152b6cc8de2e87fed
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-23 15:54:26 +0300

    Add display data from db

---
+++ oop-database/classes/test.class.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Test extends Dbh
+{
+    public function getMeals()
+    {
+        $table_name = "php_docker_table";
+        $query = "SELECT * FROM $table_name";
+        
+        $stmt = $this->connect($query);
+        while ($row = mysqli_fetch_assoc($stmt)) {
+            echo "<p>".$row['title']."</p>";
+            echo "<p>".$row['body']."</p>";
+            echo "<p>".$row['date_created']."</p>";
+            echo "<hr>";        }
+    }
+}
