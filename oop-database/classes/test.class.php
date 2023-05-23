128ce55cdd72c826864eeece2d0e16d7622f76bb
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-23 20:45:32 +0300

    Add db connection

--- oop-database/classes/test.class.php
+++ oop-database/classes/test.class.php
@@ -3,16 +3,50 @@
 
 class Test extends Dbh
 {
+    
+    
     public function getPosts()
     {
         $table_name = "php_docker_table";
-        $query = "SELECT * FROM $table_name";
+        $query = "SELECT * FROM $table_name;";
         
-        $stmt = $this->connect($query);
-        while ($row = mysqli_fetch_assoc($stmt)) {
+        $stmt = $this->connect();
+        $result = $stmt->query($query);
+        while ($row = $result->fetch_assoc()) {
             echo "<p>".$row['title']."</p>";
             echo "<p>".$row['body']."</p>";
             echo "<p>".$row['date_created']."</p>";
             echo "<hr>";        }
+            
+        // $stmt->close();
     }
+    
+    
+    
+    
+    
+    public function getPostsStmt(int $param_id)
+    {
+        
+        $table_name = "php_docker_table";
+        $query = "SELECT * FROM $table_name WHERE id = ?;";
+        $conn = $this->connect();
+        $stmt = $conn->prepare($query);
+        
+        //  $stmt->execute([$param_id]);
+        $stmt->bind_param("s", $param_id);
+         var_dump($conn);
+        $stmt->execute();     
+        // $result = $stmt->get_result();
+        
+        // foreach ($result as $row) {
+        //     echo "<p>".$row['title']."</p>";
+        //     echo "<p>".$row['body']."</p>";
+        //     echo "<p>".$row['date_created']."</p>";
+        //     echo "<hr>";
+        // }
+     
+    }
+  
+ 
 }
