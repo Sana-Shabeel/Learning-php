30306bbd2aa1aaa479181933f9848662230f1e30
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-23 07:05:30 +0300

    Add PHP database connection using PDO

--- oop-database/classes/dbh.class.php
+++ oop-database/classes/dbh.class.php
@@ -2,8 +2,20 @@
 
 class Dbh 
 {
- 
-
+    
+    private $host = "localhost";
+    private $user = "root";
+    private $pwd = "pass";
+    private $dbName = "meal_share_db";
+    
+    
+    protected function connect() 
+    {
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+        $pdo = new PDO($dsn, $this->user, $this->pwd);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    }
     
 }
 
