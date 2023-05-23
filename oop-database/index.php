feae19600a4b11c6c4f4e96d1de5f7f1a4270811
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-23 15:56:48 +0300

    Add connect db using mysqli

--- oop-database/index.php
+++ oop-database/index.php
@@ -1,5 +1,34 @@
 <?php
 
-
+include "includes/class-autoloader.inc.php";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fetch DB data</title>
+</head>
+<body>
+    
+
+    <?php
+    
+
+ 
+    
+    
+    $testObj = new Test();
+    $testObj->getMeals();
+    
+        
+     
+    ?>
+
+
+
+</body>
+</html>
