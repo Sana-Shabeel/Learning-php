31d6351578c9a6a81581fcca10ca1fc82fcb4932
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-24 08:36:37 +0300

    Add showAllUser and set new user

--- oop-mvc/index.php
+++ oop-mvc/index.php
@@ -3,3 +3,30 @@
   include 'includes/class-autoload.inc.php';
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC with OOP</title>
+</head>
+<body>
+    <h1>MVC with OOP</h1>
+    <?php
+        $usersObj = new UserView();
+        $usersObj->showAllUsers();
+        
+        echo "<br>";
+        echo "<h4>Insert new user</h4>";
+        $usersObj = new UserController();
+        // $usersObj->createUser("John", "Doe", "1980-01-01");
+        echo "<br>";
+        echo "User created successfully!"
+        
+    ?>
+    
+</body>
+</html>
